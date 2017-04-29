60f8d38944f2c36f1ef9b9b5d28ebfa2daf2c5f0
Allen Walker <lightstepdie@gmail.com>
2017-04-30 02:52:28 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -7,24 +7,26 @@ using namespace std;
 int main()
 {
 
-	ShowWindow(GetConsoleWindow(), SW_HIDE);
-	MessageBox(NULL, "Thank you for using Tea Clicker, it will be running in the background, don't worry about it, nothing's wrong it is meant to be hidden like this, to close the autoclicker simply press F4. The cps is randomized between 9 and 11 cps, you autoclick by holding your middle mouse button down. Why is this undetectable? Because it clicks exactly like a real person would, only use this autoclicker on servers with a good anti-cheat, otherwise its basically useless.", "Tea Clicker", 0);
+	ShowWindow(GetConsoleWindow(), SW_HIDE);  //hides the console
+	//gives users a messagebox with an explanation/tutorial
+	MessageBox(NULL, "Thank you for using Tea Clicker, it will be running in the background, don't worry about it, nothing's wrong it is meant to be hidden like this, to close the autoclicker simply press F4. The cps is randomized between 9 and 11 cps, you autoclick by holding your middle mouse button down. Why is this undetectable? Because it clicks exactly like a real person would.", "Tea Clicker", 0);
 
 	while(true)
 	{
-		Sleep(1);
+		Sleep(1); //dont remove this
 
-		if(GetAsyncKeyState(VK_MBUTTON))
+		if(GetAsyncKeyState(VK_MBUTTON))  //Mouse middle button (wheel click)
 		{ 
-	Sleep(28);
+			//IMPORTANT: you can remove the two Sleeps and just use Sleep(rand() % 250); which is basically 9-12 cps.
+	Sleep(28);  //this
 		mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-	Sleep(39);
+	Sleep(39); //this
 		mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 	Sleep(rand() % 59);
 		}
 
 		/*Exit the program*/
-		if (GetAsyncKeyState(0x73))
+		if (GetAsyncKeyState(0x73)) //F4
 		{
 			Sleep(100);
 			exit(0);
